fb6b3158c9ccb84b1042a8239dae341a67e2dee6
franti165 <wowwowcircle2020@gmail.com>
2022-10-26 14:04:42 +0200

    Made a python calculator

---
+++ Lesson_1/task2/task2.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+popa = input("Enter operation: ")
+if popa == "+":
+    print(num1 + num2)
+if popa == "-":
+    print(num1 - num2)
+if popa == "*":
+    print(num1 * num2)
+if popa == "/":
+    print(num1 / num2)
